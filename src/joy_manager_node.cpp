8d1628089345d3f8e1291d7ba3cac7dd1190bcd3
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-05-18 17:15:32 +0900

    update

--- src/joy_manager_node.cpp
+++ src/joy_manager_node.cpp
@@ -22,7 +22,13 @@ public:
     previous_stop_pressed_(false),
     previous_start_pressed_(false),
     speed_inverted_(false),
-    steer_inverted_(false)
+    steer_inverted_(false),
+    steer_offset_(0.0),
+    max_steer_offset_(0.2),
+    min_steer_offset_(-0.2),
+    offset_increment_(0.01),
+    increase_steer_button_index_(11),
+    decrease_steer_button_index_(12)
   {
     declare_parameter<double>("speed_scale", speed_scale_);
     declare_parameter<double>("timer_hz", timer_hz_);
@@ -34,6 +40,9 @@ public:
     declare_parameter<double>("start_axis_value", start_axis_value_);
     declare_parameter<bool>("invert_speed", speed_inverted_);
     declare_parameter<bool>("invert_steer", steer_inverted_);
+    declare_parameter<double>("max_steer_offset", max_steer_offset_);
+    declare_parameter<double>("min_steer_offset", min_steer_offset_);
+    declare_parameter<double>("offset_increment", offset_increment_);
 
     get_parameter("speed_scale", speed_scale_);
     get_parameter("timer_hz", timer_hz_);
@@ -45,6 +54,9 @@ public:
     get_parameter("start_axis_value", start_axis_value_);
     get_parameter("invert_speed", speed_inverted_);
     get_parameter("invert_steer", steer_inverted_);
+    get_parameter("max_steer_offset", max_steer_offset_);
+    get_parameter("min_steer_offset", min_steer_offset_);
+    get_parameter("offset_increment", offset_increment_);
 
     joy_sub_ = create_subscription<sensor_msgs::msg::Joy>(
       "/joy", 10, std::bind(&JoyManagerNode::joyCallback, this, _1)
@@ -73,6 +85,14 @@ private:
     joy_control_active_ = (msg->buttons[joy_button_index_] == 1);
     ackermann_control_active_ = (msg->buttons[ack_button_index_] == 1);
 
+    // Steer offset adjustment
+    if (msg->buttons[increase_steer_button_index_] == 1) {
+      steer_offset_ = std::min(steer_offset_ + offset_increment_, max_steer_offset_);
+    }
+    if (msg->buttons[decrease_steer_button_index_] == 1) {
+      steer_offset_ = std::max(steer_offset_ - offset_increment_, min_steer_offset_);
+    }
+
     bool stop_pressed = (msg->axes.size() > (size_t)stop_axis_index_ && msg->axes[stop_axis_index_] == stop_axis_value_);
     if (stop_pressed && !previous_stop_pressed_) {
       std_msgs::msg::Bool trigger_msg;
@@ -91,6 +111,7 @@ private:
 
     if (joy_control_active_) {
       current_drive_.steering_angle = steer_inverted_ ? -msg->axes[0] : msg->axes[0];
+      current_drive_.steering_angle += steer_offset_;  // <-- オフセットを追加
       current_drive_.speed = speed_inverted_ ? -msg->axes[4] * speed_scale_ : msg->axes[4] * speed_scale_;
     }
   }
@@ -113,6 +134,7 @@ private:
   rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr rosbag_trigger_pub_;
   rclcpp::TimerBase::SharedPtr timer_;
   ackermann_msgs::msg::AckermannDrive current_drive_;
+
   bool joy_control_active_;
   bool ackermann_control_active_;
   double speed_scale_;
@@ -127,6 +149,13 @@ private:
   bool previous_start_pressed_;
   bool speed_inverted_;
   bool steer_inverted_;
+
+  double steer_offset_;
+  double max_steer_offset_;
+  double min_steer_offset_;
+  double offset_increment_;
+  int increase_steer_button_index_;
+  int decrease_steer_button_index_;
 };
 
 int main(int argc, char **argv) {
